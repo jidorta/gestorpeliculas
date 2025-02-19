982e3c1687d45bbb22235b2c042640fad128ab92
Jose Iban <ibandorta@gmail.com>
2025-02-19 09:08:44 +0100

    Logro: Crear proyecto  de Gestión de peliculas

---
+++ src/main/java/net/ibandorta/projects/GestorPeliculas/GestorPeliculasApplication.java
@@ -0,0 +1,13 @@
+package net.ibandorta.projects.GestorPeliculas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorPeliculasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorPeliculasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/ibandorta/projects/GestorPeliculas/GestorPeliculasApplicationTests.java
@@ -0,0 +1,13 @@
+package net.ibandorta.projects.GestorPeliculas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorPeliculasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
